0a4fb99b21a36d612e6529a7af8e1a65f9ac5dd4
Danny Quispe Cjuiro <dquispecj@ulasalle.edu.pe>
2025-09-10 14:44:35 -0500

    Avanzando ...

--- CicloDeVidaApp/app/src/main/java/com/example/ciclodevidaapp/MainActivity.kt
+++ CicloDeVidaApp/app/src/main/java/com/example/ciclodevidaapp/MainActivity.kt
@@ -1,13 +1,76 @@
 package com.example.ciclodevidaapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.wear.compose.material.Button
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private var contador = 0
+    private lateinit var textViewContador: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activiy_main)
 
+        textViewContador = findViewById(R.id.textViewContador)
+        val btnAumentar = findViewById<Button>(R.id.btnAumentar)
+
+        btnAumentar.setOnClickListener {
+            contador++
+            textViewContador.text = "Contador: $contador"
+        }
+
+        Log.d("CICLO", "onCreate llamado")
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show()
+    }
+
+
+    // Completar esto
+    override fun onStart() {
+        super.onStart()
+        Log.d("CICLO", "onStart llamado")
+        Toast.makeText(this, "onStart()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("CICLO", "onResume llamado")
+        Toast.makeText(this, "onResume()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("CICLO", "onPause llamado")
+        Toast.makeText(this, "onPause()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("CICLO", "onStop llamado")
+        Toast.makeText(this, "onStop()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("CICLO", "onDestroy llamado")
+        Toast.makeText(this, "onDestroy()", Toast.LENGTH_SHORT).show()
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // Guarda el valor en objeto Bundle, para recuperarlo después.
+        outState.putInt("CONTADOR", contador)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Recupera la información, esta es la importancia del objeto Bundle.
+        contador = savedInstanceState.getInt("CONTADOR")
+        textViewContador.text = "Contador: $contador"
     }
 }
