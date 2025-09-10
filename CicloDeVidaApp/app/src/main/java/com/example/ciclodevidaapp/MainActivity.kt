9209c8e52103b9d31c68d7d8b5e7474e680fd227
Danny Quispe Cjuiro <dquispecj@ulasalle.edu.pe>
2025-09-10 14:32:42 -0500

    Practica Activiada

---
+++ CicloDeVidaApp/app/src/main/java/com/example/ciclodevidaapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ciclodevidaapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activiy_main)
+
+    }
+}
