1336b8bb0981c9067d0ef6609535abbf7322d330
Danny Quispe Cjuiro <dquispecj@ulasalle.edu.pe>
2025-09-22 23:23:05 -0500

    Ejercicio2 - logica Completado

--- Practica_04/P4_ejercicio02/app/src/main/java/com/example/p4_ejercicio02/EditorActivity.kt
+++ Practica_04/P4_ejercicio02/app/src/main/java/com/example/p4_ejercicio02/EditorActivity.kt
@@ -1,7 +1,12 @@
 package com.example.p4_ejercicio02
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,12 +14,41 @@ import androidx.core.view.WindowInsetsCompat
 class EditorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_editor)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // referencia de los elementos de la interfaz
+        val txtNota = findViewById<EditText>(R.id.txtNota1)
+        val btnCompartir = findViewById<Button>(R.id.btnCompartir)
+
+        // registro de un lanzador para iniciar otra actividady esperar resultado
+
+        val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+            // verificamos si la actividad siguiente devolvió un resultado
+            if (result.resultCode == RESULT_OK){
+                val data = result.data
+                val mensage = data?.getStringExtra("RESULT")
+
+                Toast.makeText(this,mensage, Toast.LENGTH_LONG).show()
+
+                //limpiar el campo
+                txtNota.setText("")
+            }
         }
+
+        //accion al presionar el boton compartir
+        btnCompartir.setOnClickListener {
+            val nota = txtNota.text.toString()
+
+            // crear el intent para iniciar una nueva actividad
+            val enviar = Intent(this, OpcionesActivity::class.java)
+
+            // agregamos datos al intent
+            enviar.putExtra("NOTA",nota)
+
+            // lanzar la nueva actividad usando el launcher de arriba
+            launcher.launch(enviar)
+        }
+
     }
 }
--- Practica_04/P4_ejercicio02/app/src/main/java/com/example/p4_ejercicio02/OpcionesActivity.kt
+++ Practica_04/P4_ejercicio02/app/src/main/java/com/example/p4_ejercicio02/OpcionesActivity.kt
@@ -1,6 +1,9 @@
 package com.example.p4_ejercicio02
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,39 @@ import androidx.core.view.WindowInsetsCompat
 class OpcionesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_opciones)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // referenciar a los elementos de la interfaz
+        val txtMensage = findViewById<TextView>(R.id.txtMensage)
+        val btnComparirCorreo = findViewById<Button>(R.id.btnCompartirCorreo)
+        val btnEditar = findViewById<Button>(R.id.btnEditar)
+
+        // recuperar los datos enviardos desde la otra actividad
+        val nota = intent.getStringExtra("NOTA")
+
+        // Se mutra los resultados
+        txtMensage.text = nota
+
+        // accion al presionar el boton editar
+        btnEditar.setOnClickListener {
+            // finaliza la actividad actual y regrea a la anterior
+            finish()
         }
+
+        // accion al presionar el boton compartir por correo
+
+        btnComparirCorreo.setOnClickListener {
+            // crear un inter vacio
+
+            val mensage = Intent()
+
+            // se añade el mensage de tipo string al intent
+            mensage.putExtra("RESULT", "Compartido por correo")
+
+            // establecer el resultado como exitoso
+            setResult(RESULT_OK,mensage)
+            finish()
+        }
+
     }
 }
