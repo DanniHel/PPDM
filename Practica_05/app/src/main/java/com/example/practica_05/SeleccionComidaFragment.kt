bcb521ad750623ea230ef05b64ef63eb58c5cdc2
Danny Quispe Cjuiro <dquispecj@ulasalle.edu.pe>
2025-10-03 06:45:03 -0500

    intefaz Selección Comida

---
+++ Practica_05/app/src/main/java/com/example/practica_05/SeleccionComidaFragment.kt
@@ -0,0 +1,11 @@
+package com.example.practica_05
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SeleccionComidaFragment : Fragment(R.layout.fragment_seleccion_comida) {
+
+}
