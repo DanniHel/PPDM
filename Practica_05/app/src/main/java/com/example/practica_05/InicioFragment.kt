f61492acd34133a4e1e9df6be0ad32bc5f437a47
Danny Quispe Cjuiro <dquispecj@ulasalle.edu.pe>
2025-10-03 21:12:55 -0500

    configurando boton 'nuevo pedido' para ir al otro fragmento

--- Practica_05/app/src/main/java/com/example/practica_05/InicioFragment.kt
+++ Practica_05/app/src/main/java/com/example/practica_05/InicioFragment.kt
@@ -15,5 +15,15 @@ import androidx.fragment.app.Fragment
 
 
 class InicioFragment : Fragment(R.layout.fragment_inicio) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnNuevoPedido = view.findViewById<Button>(R.id.btnNuevoPedido)
 
+        btnNuevoPedido.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.iniFragmento, SeleccionComidaFragment())
+                commit()
+            }
+        }
+    }
 }
