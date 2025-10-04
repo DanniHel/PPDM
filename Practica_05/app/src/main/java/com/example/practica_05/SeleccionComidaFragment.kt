8bed0e604e42e8743d2955569f1e0c99c8ebff90
Danny Quispe Cjuiro <dquispecj@ulasalle.edu.pe>
2025-10-03 21:14:18 -0500

    obteniendo el texto del radioButton

--- Practica_05/app/src/main/java/com/example/practica_05/SeleccionComidaFragment.kt
+++ Practica_05/app/src/main/java/com/example/practica_05/SeleccionComidaFragment.kt
@@ -10,5 +10,25 @@ import android.widget.RadioButton
 import android.widget.RadioGroup
 
 class SeleccionComidaFragment : Fragment(R.layout.fragment_seleccion_comida) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        //instacias de la interfaz
+        val btnSiguiente = view.findViewById<Button>(R.id.btnSiguienteComida)
+        val opcionRadioGroup = view.findViewById<RadioGroup>(R.id.radioGroupComida)
+
+        btnSiguiente.setOnClickListener {
+            // Obtener el ID del RadioButton seleccionado
+            val selectId = opcionRadioGroup.checkedRadioButtonId
+
+            if(selectId != -1){
+                // Obtener el RadioButton a partir del ID
+                val radioBoton = view.findViewById<RadioButton>(selectId)
+
+                // Obtener el texto del RadioButton seleccionado
+                val seleccionText = radioBoton.text.toString()
+
+            }
+        }
+    }
 }
